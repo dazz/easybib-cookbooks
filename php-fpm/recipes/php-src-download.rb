0983ba54e9e739dd1f99443637355afb228ab319
till <till@php.net>
2012-01-29 03:44:46 +0100

    add PHP 5.4.0RC6

--- php-fpm/recipes/php-src-download.rb
+++ php-fpm/recipes/php-src-download.rb
@@ -1,7 +1,10 @@
 # download the source and extract it
 
+php_dl="http://www.php.net/get/php-#{node["php-fpm"][:version]}.tar.gz/from/www.php.net/mirror"
+php_dl="http://downloads.php.net/stas/php-5.4.0RC6.tar.gz"
+
 remote_file "/tmp/php-#{node["php-fpm"][:version]}.tgz" do
-  source "http://www.php.net/get/php-#{node["php-fpm"][:version]}.tar.gz/from/www.php.net/mirror"
+  source "#{php_dl}"
   checksum node["php-fpm"][:checksum]
 end
 
