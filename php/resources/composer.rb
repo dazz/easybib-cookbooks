ac1a138bd6ffb1c302f2e61aecf8aa654d8ad077
till <till@php.net>
2012-07-08 19:00:14 +0200

    WIP

--- php/resources/composer.rb
+++ php/resources/composer.rb
@@ -2,3 +2,6 @@ actions :install, :setup
 
 # deploy to is the target_directory
 attribute :deploy_to, :kind_of => String, :name_attribute => true
+
+# update the composer.phar? some people like to live on the edge!
+attribute :self_update, :kind_of => [ TrueClass, FalseClass], :default => FalseClass
